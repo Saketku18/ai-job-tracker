ae432501d4cd9a38d3554bca798bfe161c543a97
Saketku18 <saket123yadav@gmail.com>
2026-06-15 16:43:14 +0530

    Fix component filename casing

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import API from "../services/api"
-import Navbar from "../components/Navbar"
+import Navbar from "../components/navbar"
 import KanbanBoard from "../components/KanbanBoard"
 import AddApplicationModal from "../components/AddApplicationModal"
 function Dashboard() {
