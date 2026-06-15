4af43fbcab7cce9c226028cbaffd165177467d24
Saketku18 <saket123yadav@gmail.com>
2026-06-15 13:16:27 +0530

    Update backend URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://jobtrack-backend-lc3f.onrender.com",
 })
 
 API.interceptors.request.use((config) => {
